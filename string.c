0c6b8595cdaddd14c8f6fd4806e03cdf0afd6599
CKanan <kenan.ceferli@yahoo.com>
2019-04-12 08:58:10 +0400

    modifed

--- string.c
+++ string.c
@@ -4,7 +4,13 @@
 int str_length(char* str){
 return strlen(str);
 }
-
+void str_compare(char* str1,char* str2){
+    int res=strcmp(str1,str2);
+    if (res==0) 
+        printf("Strings are equal"); 
+    else 
+        printf("Strings are unequal"); 
+}
 
 int main(int argc,char* argv[]){
 
