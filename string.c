1355f35de46b214644b4a892f8d3b0fd84dea26d
CKanan <kenan.ceferli@yahoo.com>
2019-04-12 09:32:45 +0400

    last version

--- string.c
+++ string.c
@@ -1,21 +1,32 @@
 #include<stdio.h>
 #include <string.h>
 
-int str_length(char* str){
-return strlen(str);
+void str_length(char str[]){
+int length=0;
+for(int i=0;str[i] !='\0';i++)
+    length++;
+printf("Its length: %d\n",length); 
 }
-void str_compare(char* str1,char* str2){
-    int res=strcmp(str1,str2);
-    if (res==0) 
-        printf("Strings are equal"); 
-    else 
-        printf("Strings are unequal"); 
+int str_compare(char str1[],char str2[]){
+    for(int i=0;str1[i]!='\0';i++){
+        for(int j=0;str2[j]!='\0';j++)
+            if(str1[i]==str2[j])
+                return 0;
+            else return 1;
+    }         
 }
 
 int main(int argc,char* argv[]){
-
-char* str1,str2;
-printf("Give two string: \n"); scanf("%s",str1);
-
-return 0;
+    char str1[1000];
+    char str2[1000];
+    printf("Give first string: \n"); 
+    scanf("%s",str1);
+    str_length(str1); 
+    printf("Give second string: \n"); 
+    scanf("%s",str2);
+    str_length(str2); 
+    if(str_compare(str1,str2)==0) printf("Strings is equal\n");
+    else printf("Strings not equal\n");
+    
+    return 0;
 }
